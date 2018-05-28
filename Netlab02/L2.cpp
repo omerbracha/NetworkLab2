d7acf09eb819f8ee518060169ea5163df8e9324a
yuvalailer <yuvalailer@gmail.com>
2018-05-28 10:03:58 +0300

    added to recvFromL2 up to type

--- Netlab02/L2.cpp
+++ Netlab02/L2.cpp
@@ -10,6 +10,11 @@
 #include <string>
 using namespace std;
 
+#define E_H_S 14 // ethernet header size
+#define ARP 0x806
+#define IP 0x800
+
+
 /**
 * Implemented for you
 */
@@ -45,9 +50,102 @@ void L2::printMsg(string msg)
 	pthread_mutex_unlock(&NIC::print_mutex);
 }
 
+/*
+* function to turn int to hex. referance to: https://stackoverflow.com/questions/5100718/integer-to-hex-string-in-c
+*/
+template< typename T >
+std::string int_to_hex(T i)
+{
+	std::stringstream stream;
+	stream << "0x"
+		<< std::setfill('0') << std::setw(sizeof(T) * 2)
+		<< std::hex << i;
+	return stream.str();
+}
+
 int L2::recvFromL2(byte *recvData, size_t recvDataLen)
 {
-		
+	string sourceMac = "";		// source mac address
+	string destMac = "";		// dest mac address
+	string userMac = "";		// user mac
+	string tmp = "";			// temp string
+	NIC nic = L2::getNIC();		// get NIC 
+	word type = 0x0;			// mesage type
+	int newSize = 0;			// extracted data size
+	byte* data;					// extraced data
+	
+
+	//start handeling header: 
+	if (debug) {
+		printMsg("recived Msg from L2:");	
+	}
+
+	// get source mac adress:
+	for (size_t i = 0; i < 6; i++)
+	{
+		sourceMac = sourceMac + std::to_string( recvData[i] ) + ":" ;
+	}
+	sourceMac.erase(-1,1); // trim end
+	
+	// get dest mac adress:
+	for (size_t i = 6; i < 12; i++)
+	{
+		destMac = destMac + std::to_string( recvData[i] ) + ":";
+	}
+	destMac.erase(-1,1); // trim end 
+	
+	if (debug) {
+		printMsg("source MAC address is: " + sourceMac);
+		printMsg("dest MAC address is: " + destMac);
+	}
+
+	// get user mac ()
+	userMac = nic.myMACAddr;
+	
+	if (debug) {
+		printMsg("user MAC address is: " + userMac);
+	}
+	
+	printMsg("debug - bolet to compere first numbers, see the HEX vs DEC");
+	
+	// check if it's not to the user
+	if (userMac.compare(destMac) != 0){
+		if (debug){
+			printMsg("msg not to user. not continued.");
+		}
+		return 0;
+	}
+
+	// check if it's from the user:
+	if (userMac.compare(sourceMac) == 0){
+		if (debug){
+			printMsg("msg originated by user. not continued.");
+		}
+		return 0;
+	}
+
+	// get msg type
+	type = type + recvData[12]; // upper
+	type = type << 8; 
+	type = type + recvData[13]; // lower
+	
+	if (debug){
+		printMsg("type is: " + );
+	}
+
+	// extract data from msg
+	newSize = recvDataLen - E_H_S;
+	data = new byte[newSize];
+	memcpy(data, recvData + E_H_S, newSize);
+
+	// send to correct part
+	if (type == ARP){
+		 ( nic.getARP() )->inarpinput(data, newSize);
+
+	}
+
+
+
 	return 42;
 }
 
